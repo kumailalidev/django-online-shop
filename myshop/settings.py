b42a14fb5e49d8eaabc8700752f3665fea74e9ca
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-22 22:33:22 +0500

    Ch-09-Adding Stripe to your project

--- myshop/settings.py
+++ myshop/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ki=!hc7$ys@6-i$k2*&1gv$xc8l&h(kg5$b+1rvs$u8gq(rj+%"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -134,3 +139,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CART_SESSION_ID = "cart"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Stripe settings
+STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY")  # Publishable key
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")  # Secret key
+STRIPE_API_VERSION = os.environ.get("STRIPE_API_VERSION")
