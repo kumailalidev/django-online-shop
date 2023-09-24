8750e12c6309515ea3fdf679d20dc6b124cf5667
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-24 06:43:46 +0500

    Ch-09-Sending PDF files by email

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import Order
+from .tasks import payment_completed
 
 
 @csrf_exempt
@@ -34,5 +35,7 @@ def stripe_webhook(request):
             # store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
+            # Launch asynchronous task (Requires to run RabbitMQ and celery)
+            payment_completed.delay(order.id)
 
     return HttpResponse(status=200)
