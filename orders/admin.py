915bbd07276ed2f5f0539c9c6e6511c5d10a374f
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-24 06:03:47 +0500

    Ch-09-Rendering PDF Files

--- orders/admin.py
+++ orders/admin.py
@@ -52,6 +52,14 @@ def order_detail(obj):
     return mark_safe(f"<a href='{url}'>View</a>")
 
 
+def order_pdf(obj):
+    url = reverse("orders:admin_order_pdf", args=[obj.id])
+    return mark_safe(f"<a href='{url}'>PDF</a>")
+
+
+order_pdf.short_description = "Invoice"
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ["product"]
@@ -72,6 +80,7 @@ class OrderAdmin(admin.ModelAdmin):
         "created",
         "updated",
         order_detail,
+        order_pdf,
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
